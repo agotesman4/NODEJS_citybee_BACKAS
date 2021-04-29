247e1e0965eb8e3c8aaff67f70bbdf8b6ecd5bbe
Adomas <you@example.com>
2021-04-29 15:24:13 +0300

    few changes more GET/POST done and tested

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ app.get("/models", async (req, res) => {
     return res.status(500).end({ error: "ERROR, PLEASE TRY AGAIN" });
   }
 });
+// OK WORKS
 app.post("/models", async (req, res) => {
   if (
     !req.body.name ||
@@ -41,22 +42,66 @@ app.post("/models", async (req, res) => {
   }
   try {
     const con = await mysql.createConnection(mysqlConfig);
-    const result = await con.execute(
-      `INSERT INTO models (name, hour_price) VALUES (${mysql.escape(
-        req.body.name
-      )}, ${mysql.escape(req.body.hour_price)})`
+    const [result] = await con.execute(
+      `INSERT INTO models (name, hour_price) VALUES 
+      (${mysql.escape(req.body.name)}, 
+      ${mysql.escape(req.body.hour_price)})`
     );
-    res.send(result);
+
+    if (!result.insertId) {
+      return res.status(500).send({
+        error: "MORTAL KOMBAT ERROR contact 112 for MORE INFO",
+      });
+    }
+    return res.send({ id: result.insertId });
   } catch (err) {
     console.log(err);
     return res.status(500).send({ error: "ERROR, PLEASE TRY AGAIN" });
   }
 });
+// OK WORKS
 
-app.get("/modelscount", async (req, res) => {});
+app.get("/modelscount", async (req, res) => {
+  try {
+    const con = await mysql.createConnection(mysqlConfig);
+    const [data] = await con.execute(
+      `SELECT COUNT(name) AS TotalOfCars FROM models`
+    );
+
+    return res.send(data);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).end({ error: "ERROR, PLEASE TRY AGAIN" });
+  }
+});
+// NEED SOME FIXING
 
 app.get("/vehicles", async (req, res) => {});
-app.post("/vehicles", async (req, res) => {});
+// GET NOT DONE
+app.post("/vehicles", async (req, res) => {
+  if (
+    !req.body.model_id ||
+    !req.body.number_plate ||
+    !req.body.country_location
+  ) {
+    return res.status(400).send({ error: "BAD DATA GIVEN" });
+  }
+  try {
+    const con = await mysql.createConnection(mysqlConfig);
+    const result = await con.execute(
+      `INSERT INTO vehicles (model_id, number_plate, country_location) VALUES (${mysql.escape(
+        req.body.model_id
+      )}, ${mysql.escape(req.body.number_plate)}, ${mysql.escape(
+        req.body.country_location
+      )})`
+    );
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ error: "ERROR, TRY LATER" });
+  }
+});
+// OK WORKS, BUT NEED SOME IMPROVEMENT
 
 app.get("/vehicles/lt", async (req, res) => {});
 app.get("/vehicles/lv", async (req, res) => {});
